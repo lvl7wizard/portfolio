7d8d3f375fe2bce07c043f8acdef4204c72af1a7
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-02 14:21:13 +0100

    change layout and colour scheme

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,12 +5,12 @@ export const About = () => {
   return (
     <section className={styles.container} id="about">
       <h2 className={styles.title}>ABOUT</h2>
-      <p>
+      <p className={styles.scrollable}>
         I picked up coding as a hobby in 2020. I love the combination of making
         things 🎨, problem solving 🕹️, and learning 💡. <br />
         <br />
         After facing a redunancy in October 2023, I took the opportunity to take
-        coding to the next level by enrolling for a 3-month full-time Software
+        coding to take my hobby to the next level by enrolling for a 3-month full-time Software
         Engineering bootcamp with a highly-rated organisation called
         Northcoders. <br />
         <br />
@@ -18,7 +18,7 @@ export const About = () => {
         backend web development and granted me hands on experience with popular
         industry practices and techniques. <br />
         <br />
-        I'm now looking for a role where I can utilise my skills and continue
+        I'm now looking for a professional role where I can utilise my skills and continue
         learning.
       </p>
       <div className={styles.btnContainer}>
