da27ac5df5b624ef28376662047fd5724b3f4e9d
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-08 09:32:10 +0100

    add Carousel to Projects section for larger screens

---
+++ src/components/Projects/Carousel.jsx
@@ -0,0 +1,22 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import projects from "../../data/projects.json";
+import { ProjectCard } from "./ProjectCard";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 831 },
+    items: 1,
+    slidesToSlide: 1,
+  }
+};
+
+export const ProjectCarousel = () => {
+  return (
+    <Carousel responsive={responsive} showDots={true} infinite={true}> 
+      {projects.map((project, id) => {
+        return <ProjectCard key={id} project={project}/>
+      })}
+    </Carousel>
+  );
+};
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,24 @@
 import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
 import styles from "./Projects.module.css"
+import { ProjectCarousel } from "./Carousel";
+import { useState, useEffect } from "react";
 
 export const Projects = () => {
+  // Carousel media query applied within this component as ProjectCarousel has built in styling which causes issues with CSS conditional rendering
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 831);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 831);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <section className={styles.container} id={"projects"}>
       <div>
@@ -13,6 +29,7 @@ export const Projects = () => {
           return <ProjectCard key={id} project={project}/>
         })}
       </div>
+      {isLargeScreen && <ProjectCarousel className={styles.carousel}/>}
       <div className={styles.btnContainer}>
         <a href="#" className={styles.toTheTopBtn}>
           To the top ↑
