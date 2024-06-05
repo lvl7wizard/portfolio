3748cc9bc3bc88eec90970b7700f5afa25648b56
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-06-06 00:14:35 +0100

    refactors Projects component to use tailwind

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,40 +1,21 @@
 import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
-import styles from "./Projects.module.css"
-import { ProjectCarousel } from "./Carousel";
-import { useState, useEffect } from "react";
 
 export const Projects = () => {
-  // Carousel media query applied within this component as ProjectCarousel has built in styling which causes issues with CSS conditional rendering
-  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 831);
-  useEffect(() => {
-    const handleResize = () => {
-      setIsLargeScreen(window.innerWidth >= 831);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
-    <section className={styles.container} id={"projects"}>
-      <div>
-      <h2 className={styles.title}>Projects</h2>
-      </div>
-      <div className={styles.projects}>
+    <section className="flex flex-col min-h-screen mx-5vw items-center justify-center text-center content-center text-lg text-white gap-8" id={"projects"}>
+      <h2 className="text-8xl font-bold text-blue-400 text-shadow-3d-blue">Projects</h2>
         {projects.map((project, id) => {
           return <ProjectCard key={id} project={project}/>
         })}
-      </div>
-      {isLargeScreen && <ProjectCarousel className={styles.carousel}/>}
-      <div className={styles.btnContainer}>
-        <a href="#" className={styles.toTheTopBtn}>
-          To the top ↑
+        <a href="#">
+          <button
+            type="button"
+            className="text-white bg-gradient-to-r from-yellow-500 to-orange-600 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-red-200 dark:focus:ring-red-800 font-medium rounded-lg px-6 py-3 text-center mb-8"
+          >
+            To the top ↑
+          </button>
         </a>
-        </div>
     </section>
   );
 };
