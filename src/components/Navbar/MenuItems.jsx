3b0a123cc90c235db2273de2a278b1819cd98491
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-06-06 00:19:45 +0100

    creates MenuItems as part of Navbar refactor

---
+++ src/components/Navbar/MenuItems.jsx
@@ -0,0 +1,26 @@
+import {Close} from "@mui/icons-material"
+
+const MenuItems = ({showMenu, active}) => {
+  return (
+    <ul className={active ? "flex-col flex items-center fixed inset-0 justify-center bg-black/50 backdrop-blur-sm gap-8 p-8 text-2xl md:hidden" : "hidden"}>
+        <Close className="scale-150" onClick={showMenu}/>
+        <li>
+            <a href="#">Home</a>
+          </li>
+      <li>
+        <a href="#about">About</a>
+      </li>
+      <li>
+        <a href="#skills">Skills</a>
+      </li>
+      <li>
+        <a href="#projects">Projects</a>
+      </li>
+      <li>
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
+  );
+};
+
+export default MenuItems;
