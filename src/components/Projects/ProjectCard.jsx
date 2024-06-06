fa2cf51f481902305ef846363eb0196b8dd91d41
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-06-06 13:22:21 +0100

    refactors projectcard layout

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -4,43 +4,51 @@ export const ProjectCard = ({
   project: { title, imageSrc, description, skills, demo, source },
 }) => {
   return (
-    <div className="p-6 flex flex-col items-center lg:items-start lg:flex-row lg:flex-wrap lg:justify-center">
-      <h3 className="text-6xl font-bold text-white text-shadow-3d-white p-10 w-full lg:text-center">
+    <div className="flex flex-col items-center">
+      <h3 className="text-5xl font-bold text-white text-shadow-3d-white p-5 w-full">
         {title}
       </h3>
-      <div className="lg:w-1/2 lg:pr-6">
-        <a href={demo}>
+        <a href={demo} target="_blank" rel="noopener noreferrer" className="flex justify-center">
           <img
             src={getImageUrl(imageSrc)}
             alt={`image of ${title}`}
-            className="h-auto rounded-md w-full max-w-screen-sm"
+            className="w-11/12 rounded-md max-w-lg"
           />
         </a>
+      <div className="mt-5">
+        <h3 className="text-lg font-semibold">Description:</h3>
+        <p className="text-md">{description}</p>
       </div>
-      <div className="mt-4 lg:w-1/2 lg:pl-6 lg: self-center">
-        <h3 className="text-lg font-semibold mb-2">Description:</h3>
-        <p className="text-sm mb-4">{description}</p>
+      <div className="mt-2">
+        <h3 className="text-lg font-semibold">Links:</h3>
+        <div className="flex gap-4 justify-center mt-2">
+          <a href={demo} target="_blank" rel="noopener noreferrer">
+            <button
+              type="button"
+              className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transform motion-safe:hover:scale-110"
+            >
+              Demo
+            </button>
+          </a>
+          <a href={source} target="_blank" rel="noopener noreferrer">
+            <button
+              type="button"
+              className="text-white bg-gradient-to-br from-orange-400 to-pink-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transform motion-safe:hover:scale-110"
+            >
+              Source
+            </button>
+          </a>
+        </div>
       </div>
-      <div className="mt-4 lg:w-1/2 lg:pr-6">
-        <h3 className="text-lg font-semibold mb-2">Tech Stack:</h3>
-        <ul className="flex flex-wrap gap-2 mb-4 justify-center">
+      <div className="mt-2">
+        <h3 className="text-lg font-semibold">Tech Stack:</h3>
+        <ul className="flex flex-wrap gap-2 mt-2 justify-center">
           {skills.map((skill, id) => (
-            <li key={id} className="bg-blue-700 rounded-full px-3 py-1 text-sm">
+            <li key={id} className="bg-blue-700 rounded-full px-3 py-1 text-sm transform motion-safe:hover:scale-110">
               {skill}
             </li>
           ))}
         </ul>
-      </div>
-      <div className="mt-4 lg:w-1/2 lg:pl-6">
-        <h3 className="text-lg font-semibold mb-2">Links:</h3>
-        <div className="flex gap-4 justify-center">
-          <a href={demo} className="bg-red-600 rounded-full px-3 py-1 text-sm hover:underline">
-            Demo
-          </a>
-          <a href={source} className="bg-red-600 rounded-full px-3 py-1 text-sm hover:underline">
-            Source
-          </a>
-        </div>
       </div>
     </div>
   );
